a8a765d62cf9df902214405c13f9eac8d2c33397
Siddhant Jain <138900970+jainsiddhantofficial@users.noreply.github.com>
2026-06-02 22:24:50 +0530

    Time: 1 ms (100%), Space: 47.1 MB (9.92%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.java
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.java
@@ -1,32 +1,19 @@
 class Solution {
-    public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
-        int res = Integer.MAX_VALUE;
-        int n = landStartTime.length;
-        int m = waterStartTime.length;
+    public int earliestFinishTime(int[] startL, int[] durL, int[] startW, int[] durW) {
+        int minL = 3000, minW = minL, res = minW;
+        int n = startL.length;
+        int m = startW.length;
 
-        // Try all combinations of one land and one water ride
-        for (int i = 0; i < n; i++) {
-            int a = landStartTime[i]; // start time of land ride
-            int d = landDuration[i];  // duration of land ride
+        for (int i = 0; i < n; i++)
+            minL = Math.min(minL, startL[i] + durL[i]);
 
-            for (int j = 0; j < m; j++) {
-                int b = waterStartTime[j]; // start time of water ride
-                int e = waterDuration[j];  // duration of water ride
-
-                // Case 1: Land → Water
-                int landEnd = a + d;
-                int startWater = Math.max(landEnd, b); // wait if needed
-                int finish1 = startWater + e;
-
-                // Case 2: Water → Land
-                int waterEnd = b + e;
-                int startLand = Math.max(waterEnd, a); // wait if needed
-                int finish2 = startLand + d;
-
-                // Take the minimum finish time
-                res = Math.min(res, Math.min(finish1, finish2));
-            }
+        for (int i = 0; i < m; i++) {
+            minW = Math.min(minW, startW[i] + durW[i]);
+            res = Math.min(res, Math.max(minL, startW[i]) + durW[i]);
         }
+
+        for (int i = 0; i < n; i++)
+            res = Math.min(res, Math.max(minW, startL[i]) + durL[i]);
 
         return res;
     }
